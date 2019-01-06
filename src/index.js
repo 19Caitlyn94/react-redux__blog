a784a51c456e0d29fd2cc209e6dc741438c1587b
1Caitlyn1 <caitlyn@gradconnection.com>
2019-01-06 22:50:24 +0200

    Setting up React-Router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
+import PostsIndex from './components/posts_index' 
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Home extends React.Component {
+  render() { return <div>Home!</div> }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <BrowserRouter>
+  <div>
+    <Route exact path="/" component={PostsIndex} />
+    </div>
+  </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
